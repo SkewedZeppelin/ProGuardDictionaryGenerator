c7664592e54b99fb4bdc70a532d40d1d45286414
Tad <tad@spotco.us>
2016-05-03 21:44:40 -0400

    Improve randomness and character checking

--- src/info/spotcomms/proguarddictionarygenerator/Main.java
+++ src/info/spotcomms/proguarddictionarygenerator/Main.java
@@ -2,27 +2,65 @@ package info.spotcomms.proguarddictionarygenerator;
 
 import java.io.File;
 import java.io.PrintWriter;
-import java.security.SecureRandom;
-import java.util.Random;
+import java.util.Locale;
+
+import org.uncommons.maths.random.AESCounterRNG;
+import org.uncommons.maths.random.DevRandomSeedGenerator;
+import org.uncommons.maths.random.SecureRandomSeedGenerator;
+import org.uncommons.maths.random.SeedGenerator;
 
 public class Main {
 
+  private static SeedGenerator seedGenerator = null;
+  private static AESCounterRNG secureRandom = null;
+  private static char[] VALID_CHARACTERS = null;
+
   public static void main(String[] args) {
-    System.out.println("Control Code Test: " + check('\u0000'));
-    System.out.println("Example String: " + csRandomAlphaNumericString(256));
+    initializeRandoms();
+    VALID_CHARACTERS = generateRandomUnicodeArray();
+    System.out.println("Example String: " + generateRandomUnicodeString(256));
     createDatabase("ObfuscationDictionary.txt", 2500, 20000);
     createDatabase("ClassObfuscationDictionary.txt", 5000, 250);
     createDatabase("PackageObfuscationDictionary.txt", 5000, 250);
   }
 
+  private static void initializeRandoms() {
+    try {
+      String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+      if(os.equals("Linux") || os.equals("Mac")) {
+        seedGenerator = new DevRandomSeedGenerator();
+      } else {
+        seedGenerator = new SecureRandomSeedGenerator();
+      }
+      updateSeed();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void updateSeed() {
+    try {
+      try {
+        secureRandom = new AESCounterRNG(seedGenerator.generateSeed(32));
+      } catch(Exception e) {
+        secureRandom = new AESCounterRNG(seedGenerator.generateSeed(16));
+        e.printStackTrace();
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
   private static void createDatabase(String name, int amount, int lineLength) {
     try {
+      initializeRandoms();
       System.out.println("Starting generation of " + name);
       PrintWriter writer = new PrintWriter(new File(name));
       for (int x = 0; x < amount; x++) {
-        writer.println(csRandomAlphaNumericString(lineLength));
-        if (x % 50 == 0) {
-          VALID_CHARACTERS = generateRandomArray();
+        writer.println(generateRandomUnicodeString(lineLength));
+        if (x % 25 == 0) {
+          updateSeed();
+          VALID_CHARACTERS = generateRandomUnicodeArray();
         }
         if(x % 250 == 0) {
           System.out.println("\tCompletion: " + x + "/" + amount);
@@ -35,21 +73,12 @@ public class Main {
     }
   }
 
-  private static boolean check(char c) {
-    if (Character.isLetter(c) || Character.isJavaIdentifierStart(c)) {
-      return true;
-    }
-    return false;
-  }
-
-  private static char[] generateRandomArray() {
+  private static char[] generateRandomUnicodeArray() {
     char[] out = new char[65536];
-    Random rand = new Random();
     for (int x = 0; x < out.length; x++) {
-      rand.setSeed(new SecureRandom().nextLong() * System.nanoTime());
-      char tmp = (char) Integer.parseInt(("\\u" + Integer.toHexString(rand.nextInt(0x10FFFF))).substring(2), 16);
-      if (check(tmp)) {
-        out[x] = tmp;
+      int uchar = Integer.parseInt(("\\u" + Integer.toHexString(secureRandom.nextInt(0x10FFFF))).substring(2), 16);
+      if(Character.isDefined(uchar)) {
+        out[x] = (char) uchar;
       } else {
         x--;
       }
@@ -57,17 +86,10 @@ public class Main {
     return out;
   }
 
-  private static char[] VALID_CHARACTERS = generateRandomArray();
-
-  public static String csRandomAlphaNumericString(int numChars) {
-    SecureRandom srand = new SecureRandom();
-    Random rand = new Random();
+  public static String generateRandomUnicodeString(int numChars) {
     char[] buff = new char[numChars];
     for (int i = 0; i < numChars; ++i) {
-      if ((i % 10) == 0) {
-        rand.setSeed(srand.nextLong());
-      }
-      buff[i] = VALID_CHARACTERS[rand.nextInt(VALID_CHARACTERS.length)];
+      buff[i] = VALID_CHARACTERS[secureRandom.nextInt(VALID_CHARACTERS.length)];
     }
     return new String(buff);
   }
